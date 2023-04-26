ac4bb096760d618d57aa323b87f9b43ab8020242
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-04-26 09:08:08 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,27 @@ basic.forever(function () {
     distoncijswhamgvdfzj = maqueen.Ultrasonic(PingUnit.Centimeters)
 })
 basic.forever(function () {
-    if (distoncijswhamgvdfzj == 15) {
+    if (distoncijswhamgvdfzj < 16) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
         maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
-    if (distoncijswhamgvdfzj < 15) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
-    }
-    if (distoncijswhamgvdfzj > 15) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
 })
