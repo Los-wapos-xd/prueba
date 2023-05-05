633269e5c16c1f58f5669e4f7c591557134cf296
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-05 11:35:53 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,3 +27,8 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
 })
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
+})
