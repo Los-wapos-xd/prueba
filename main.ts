edf0303268c612dafd865e2d758c061ab97c0976
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-09 11:45:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,12 @@ basic.forever(function () {
         if (maqueen.Ultrasonic(PingUnit.Centimeters) < 25) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
         } else {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 70)
         }
     } else {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 2 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 2) {
-            basic.pause(700)
-            maqueen.motorStop(maqueen.Motors.All)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-        }
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        maqueen.motorStop(maqueen.Motors.M2)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 70)
     }
 })
